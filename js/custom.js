8e94dc9e47600d288ebc1e2cc502fc57c99e187f
vaibhav goyal <vgoyal20@iitk.ac.in>
2022-09-12 01:48:07 +0530

    changes done

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,83 @@
-(function($) {
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
+const counter = document.querySelectorAll(".counter");
+let flag = 0;
+document.addEventListener("scroll", function () {
+  if (!flag && isInViewport(counter[0])) {
+    flag = 1;
+    let counts = setInterval(updated);
+    let upto = 0;
+    function updated() {
+      for (e of counter) {
+        e.innerHTML = upto + "+";
+        if (upto === 1000) {
+          clearInterval(counts);
+        }
+      }
+      upto += 2;
+    }
+  }
+});
+
+(function ($) {
   // Init Wow
   wow = new WOW({
-    animateClass: 'animated',
-    offset: 100
+    animateClass: "animated",
+    offset: 100,
   });
   wow.init();
 
-  $(".navbar-collapse a").on('click', function() {
-    $(".navbar-collapse.collapse").removeClass('in');
+  $(".navbar-collapse a").on("click", function () {
+    $(".navbar-collapse.collapse").removeClass("in");
   });
 
   // Navigation scrolls
-  $('.navbar-nav li a').bind('click', function(event) {
-    $('.navbar-nav li').removeClass('active');
-    $(this).closest('li').addClass('active');
+  $(".navbar-nav li a").bind("click", function (event) {
+    $(".navbar-nav li").removeClass("active");
+    $(this).closest("li").addClass("active");
     var $anchor = $(this);
-    var nav = $($anchor.attr('href'));
+    var nav = $($anchor.attr("href"));
     if (nav.length) {
-      $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top,
+          },
+          1500,
+          "easeInOutExpo"
+        );
 
       event.preventDefault();
     }
   });
 
   // About section scroll
-  $(".overlay-detail a").on('click', function(event) {
+  $(".overlay-detail a").on("click", function (event) {
     event.preventDefault();
     var hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function() {
-      window.location.hash = hash;
-    });
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      900,
+      function () {
+        window.location.hash = hash;
+      }
+    );
   });
 
   //jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(".navbar-default").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
@@ -47,9 +86,8 @@
   });
 
   // Testimonials Slider
-  $('.bxslider').bxSlider({
+  $(".bxslider").bxSlider({
     adaptiveHeight: true,
-    mode: 'fade'
+    mode: "fade",
   });
-
 })(jQuery);
