4eaebf61da7251842ac6b3c5533befcab19c7abe
Kartik Soni <kartiksoni21@iitk.ac.in>
2022-10-04 15:45:41 +0530

    updates

---
+++ js/animate.js
@@ -0,0 +1,39 @@
+ScrollReveal({
+    repeat:'false',
+    distance:'8px',
+    delay:100
+}
+);
+ScrollReveal().reveal('.rightq', { delay: 1000 ,distance:'150px', origin:'right'});
+ScrollReveal().reveal('.leftq', { delay: 1000 ,distance:'150px', origin:'left'});
+ScrollReveal().reveal('.perk-box', { delay: 1000 ,distance:'150px', origin:'left'});
+ScrollReveal().reveal('.heading2', { delay: 1000 });
+ScrollReveal().reveal('.slow', { delay: 700 });
+ScrollReveal().reveal('.slow2', { delay: 900 });
+ScrollReveal().reveal('.heading3', { delay: 1500 });
+ScrollReveal().reveal('.heading4', { delay: 150 });
+ScrollReveal().reveal('.heading5', { delay: 750 });
+ScrollReveal().reveal('.heading1', { delay: 300 });
+ScrollReveal().reveal('.hsftt', { delay: 1000 });
+ScrollReveal().reveal('.col-md-4', { delay: 1000 ,distance:'150px', origin:'left'});
+ScrollReveal().reveal('.sanmati', { delay: 0});
+ScrollReveal().reveal('.aman', { delay: 500});
+ScrollReveal().reveal('.vaibhav', { delay: 1000});
+
+
+ScrollReveal().reveal('.wait1', { delay: 250 });
+ScrollReveal().reveal('.wait2', { delay: 500 });
+ScrollReveal().reveal('.wait3', { delay: 750 });
+ScrollReveal().reveal('.wait4', { delay: 1000 });
+
+
+
+
+
+
+
+
+
+
+
+
