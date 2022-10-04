08067fc6dd0d15015d622e45297ca64aa08d3c79
vaibhav goyal <vgoyal20@iitk.ac.in>
2022-10-04 16:32:09 +0530

    animations

--- js/animate.js
+++ js/animate.js
@@ -1,39 +1,43 @@
 ScrollReveal({
-    repeat:'false',
-    distance:'8px',
-    delay:100
-}
-);
-ScrollReveal().reveal('.rightq', { delay: 1000 ,distance:'150px', origin:'right'});
-ScrollReveal().reveal('.leftq', { delay: 1000 ,distance:'150px', origin:'left'});
-ScrollReveal().reveal('.perk-box', { delay: 1000 ,distance:'150px', origin:'left'});
-ScrollReveal().reveal('.heading2', { delay: 1000 });
-ScrollReveal().reveal('.slow', { delay: 700 });
-ScrollReveal().reveal('.slow2', { delay: 900 });
-ScrollReveal().reveal('.heading3', { delay: 1500 });
-ScrollReveal().reveal('.heading4', { delay: 150 });
-ScrollReveal().reveal('.heading5', { delay: 750 });
-ScrollReveal().reveal('.heading1', { delay: 300 });
-ScrollReveal().reveal('.hsftt', { delay: 1000 });
-ScrollReveal().reveal('.col-md-4', { delay: 1000 ,distance:'150px', origin:'left'});
-ScrollReveal().reveal('.sanmati', { delay: 0});
-ScrollReveal().reveal('.aman', { delay: 500});
-ScrollReveal().reveal('.vaibhav', { delay: 1000});
+  repeat: "false",
+  distance: "8px",
+  delay: 100,
+});
+ScrollReveal().reveal(".rightq", {
+  delay: 1000,
+  distance: "1500px",
+  origin: "right",
+});
+ScrollReveal().reveal(".leftq", {
+  delay: 1000,
+  distance: "150px",
+  origin: "left",
+});
+ScrollReveal().reveal(".perk-box", {
+  delay: 500,
+  distance: "150px",
+  origin: "left",
+});
+ScrollReveal().reveal(".heading2", { delay: 1000, distance: "200px" });
+ScrollReveal().reveal(".slow", { delay: 700 });
+ScrollReveal().reveal(".slow2", { delay: 900 });
+ScrollReveal().reveal(".heading3", { delay: 700, distance: "150px" });
+ScrollReveal().reveal(".heading4", { delay: 150, distance: "150px" });
+ScrollReveal().reveal("#intro-image", { delay: 750, distance: "150px" });
+ScrollReveal().reveal(".apply-box", { delay: 1200, distance: "150px" });
+ScrollReveal().reveal(".heading5", { delay: 750, distance: "150px" });
+ScrollReveal().reveal(".heading1", { delay: 300, distance: "200px" });
+ScrollReveal().reveal(".hsftt", { delay: 1000 });
+ScrollReveal().reveal(".col-md-4", {
+  delay: 1000,
+  distance: "150px",
+  origin: "left",
+});
+ScrollReveal().reveal(".sanmati", { delay: 0 });
+ScrollReveal().reveal(".aman", { delay: 250 });
+ScrollReveal().reveal(".vaibhav", { delay: 500 });
 
-
-ScrollReveal().reveal('.wait1', { delay: 250 });
-ScrollReveal().reveal('.wait2', { delay: 500 });
-ScrollReveal().reveal('.wait3', { delay: 750 });
-ScrollReveal().reveal('.wait4', { delay: 1000 });
-
-
-
-
-
-
-
-
-
-
-
-
+ScrollReveal().reveal(".wait1", { delay: 250, distance: "100px" });
+ScrollReveal().reveal(".wait2", { delay: 500, distance: "100px" });
+ScrollReveal().reveal(".wait3", { delay: 750, distance: "100px" });
+ScrollReveal().reveal(".wait4", { delay: 1000, distance: "100px" });
