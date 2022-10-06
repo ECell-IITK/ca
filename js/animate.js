e89be0e23767ca62b28c7364e7a506b56781bfef
vaibhav goyal <vgoyal20@iitk.ac.in>
2022-10-06 18:01:10 +0530

    updated associates

--- js/animate.js
+++ js/animate.js
@@ -28,11 +28,6 @@ ScrollReveal().reveal(".apply-box", { delay: 1200, distance: "150px" });
 ScrollReveal().reveal(".heading5", { delay: 750, distance: "150px" });
 ScrollReveal().reveal(".heading1", { delay: 300, distance: "200px" });
 ScrollReveal().reveal(".hsftt", { delay: 1000 });
-ScrollReveal().reveal(".col-md-4", {
-  delay: 1000,
-  distance: "150px",
-  origin: "left",
-});
 ScrollReveal().reveal(".sanmati", { delay: 0 });
 ScrollReveal().reveal(".aman", { delay: 250 });
 ScrollReveal().reveal(".vaibhav", { delay: 500 });
@@ -41,3 +36,7 @@ ScrollReveal().reveal(".wait1", { delay: 250, distance: "100px" });
 ScrollReveal().reveal(".wait2", { delay: 500, distance: "100px" });
 ScrollReveal().reveal(".wait3", { delay: 750, distance: "100px" });
 ScrollReveal().reveal(".wait4", { delay: 1000, distance: "100px" });
+
+ScrollReveal().reveal(".first", { delay: 250, distance: "200px" });
+ScrollReveal().reveal(".second", { delay: 500, distance: "200px" });
+ScrollReveal().reveal(".third", { delay: 750, distance: "200px" });
